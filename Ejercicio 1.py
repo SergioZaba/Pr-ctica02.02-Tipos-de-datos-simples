77effbc2bbf7c2a595da59237982ce4291f5c34e
SergioZaba <113203945+SergioZaba@users.noreply.github.com>
2022-10-11 12:09:04 +0200

    PRIMEROS PASOS

---
+++ Ejercicio 1.py
@@ -0,0 +1,4 @@
+nombre_usuario = input("Introduzca su nombre de usuario")
+numero_entero = int(input("¿Cuantas veces desea repetir su nombre?"))
+repeticion = nombre_usuario * numero_entero * "\n"
+print (repeticion)
